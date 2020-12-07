3814092846caf8ccfbe08ca5a4a49e287728f956
sasaso7 <57388672+sasaso7@users.noreply.github.com>
2020-12-07 09:19:20 +0100

    start til edit

--- src/main/java/edu/kea/exam/pizza/demo/controller/DishCRUDController.java
+++ src/main/java/edu/kea/exam/pizza/demo/controller/DishCRUDController.java
@@ -1,7 +1,11 @@
 package edu.kea.exam.pizza.demo.controller;
 
 import edu.kea.exam.pizza.demo.model.Dish;
+import edu.kea.exam.pizza.demo.model.Drink;
+import edu.kea.exam.pizza.demo.model.Side;
 import edu.kea.exam.pizza.demo.persistence.DishCrudRepository;
+import edu.kea.exam.pizza.demo.persistence.DrinkCrudRepository;
+import edu.kea.exam.pizza.demo.persistence.SideCrudRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,14 +16,20 @@ import org.springframework.validation.Errors;
 
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
-
 public class DishCRUDController {
-    DishCrudRepository dishCrudRepository;
 
-    public DishCRUDController(DishCrudRepository dishCrudRepository) {
-        this.dishCrudRepository = dishCrudRepository;
+    DishCrudRepository dishRepository;
+    SideCrudRepository sideRepository;
+    DrinkCrudRepository drinkRepository;
+
+
+    public DishCRUDController(DishCrudRepository dishRepository, SideCrudRepository sideRepository, DrinkCrudRepository drinkRepository) {
+        this.dishRepository = dishRepository;
+        this.sideRepository = sideRepository;
+        this.drinkRepository = drinkRepository;
     }
 
     @GetMapping("internal/new-dish")
@@ -34,10 +44,20 @@ public class DishCRUDController {
             return "crud/new-dish";
         } else {
 
-            dishCrudRepository.save(dish);
+            dishRepository.save(dish);
             System.out.println(dish);
             return "Index";
         }
+    }
+    @GetMapping("internal/edit-dish")
+    public String editDish(Model model) {
+        Iterable<Dish> dishList = dishRepository.findAll();
+        Iterable<Drink> drinkList = drinkRepository.findAll();
+        Iterable<Side> sideList = sideRepository.findAll();
+        model.addAttribute("dishList", dishList);
+        model.addAttribute("drinkList", drinkList);
+        model.addAttribute("sideList", sideList);
+        return "crud/edit-dish";
     }
 
 
