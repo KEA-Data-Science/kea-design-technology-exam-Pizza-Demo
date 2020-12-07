aef8a11836c9a6a16b134b745d22dc2c0e1c231b
Ostefar <56957832+Ostefar@users.noreply.github.com>
2020-12-07 09:36:13 +0100

    test1 - under construktion

--- src/test/java/edu/kea/exam/pizza/demo/controllers/CardControllerTest.java
+++ src/test/java/edu/kea/exam/pizza/demo/controllers/CardControllerTest.java
@@ -1,21 +1,53 @@
 package edu.kea.exam.pizza.demo.controllers;
 import edu.kea.exam.pizza.demo.controller.CardController;
+import edu.kea.exam.pizza.demo.persistence.DishCrudRepository;
+import edu.kea.exam.pizza.demo.persistence.DrinkCrudRepository;
+import edu.kea.exam.pizza.demo.persistence.SideCrudRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.ui.Model;
+import static org.mockito.Mockito.mock;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 class CardControllerTest {
 
+    @Mock
+    DishCrudRepository dishRepo;
 
-@Mock
-CardController cardController;
+    @Mock
+    SideCrudRepository sideRepo;
 
-//@BeforeEach
-void setUp(){
+    @Mock
+    DrinkCrudRepository drinkRepo;
+
+    @Mock
+    Model mockModel;
+
+    CardController cardController;  //test subject
+
+
+    // arranges data between tests
+    @BeforeEach
+    void setUp(){
+    mockModel = mock(Model.class);
+
+    dishRepo = mock(DishCrudRepository.class);
+
+
+    sideRepo = mock(SideCrudRepository.class);
+
+
+    drinkRepo = mock(DrinkCrudRepository.class);
 
 }
-    //@Test
+    // testcase
+    @Test
+    void showCard(){
+
+    }
 }
