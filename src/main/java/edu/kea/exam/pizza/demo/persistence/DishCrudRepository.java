a71a34e44b8d5442a8cb7ad9c5413eea4e09d05f
Kian Nielsen <kian.tema@gmail.com>
2020-12-10 14:05:57 +0100

    Removed zombie code in DishCrudRepository.java. (Pushing tiny changes to test Jenkins jobs.)

--- src/main/java/edu/kea/exam/pizza/demo/persistence/DishCrudRepository.java
+++ src/main/java/edu/kea/exam/pizza/demo/persistence/DishCrudRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface DishCrudRepository extends CrudRepository<Dish, Integer> {
-//    List<Dish> findAll();
+
 }
