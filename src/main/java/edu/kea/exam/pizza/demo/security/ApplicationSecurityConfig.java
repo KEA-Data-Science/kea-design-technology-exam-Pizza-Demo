e139e35438003d3779d37efea42eaf9a8f598635
Kian Nielsen <kian.tema@gmail.com>
2020-12-14 13:19:22 +0100

    Updated description of class ApplicationSecurityConfig.java. (Pushing tiny changes to test Jenkins jobs.)

--- src/main/java/edu/kea/exam/pizza/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/edu/kea/exam/pizza/demo/security/ApplicationSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+/* Class handling customization of Spring security configuration */
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
